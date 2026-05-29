c2c81915ccb43c84d5d1667294817b58b55b03ec
Crisa97 <cccruz76382@umanizales.edu.co>
2026-05-29 15:43:40 -0500

    chore: clean up temp debug scripts, move vuln test project to tests/test_projects/

---
+++ tests/test_projects/vuln_app.php
@@ -0,0 +1,9 @@
+﻿<?php
+$id = $_GET["id"];
+$q = "SELECT * FROM users WHERE id = $id";
+eval("\$page = " . $_GET["page"] . ";");
+include($_GET["file"]);
+$password = "admin123";
+$cmd = "ping " . $_POST["ip"];
+system($cmd);
+?>
